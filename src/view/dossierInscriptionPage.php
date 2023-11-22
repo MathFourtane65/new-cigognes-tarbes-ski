70cfb52f19150bfb06a8741f672d7e3e2023ba3e
Mathieu Fourtané <mathieu.fourtane@gmail.com>
2023-11-22 19:14:56 +0100

    fichiers dossiers d'inscriptions

--- src/view/dossierInscriptionPage.php
+++ src/view/dossierInscriptionPage.php
@@ -19,8 +19,11 @@
         <div class="row">
             <div class="col text-center">
                 <p class="lead mb-5">Nous vous invitons à télécharger les formulaires requis pour l'inscription à la prochaine saison. Les documents sont mis à jour et disponibles au téléchargement ci-dessous.</p> <!-- <a href="/fichiers/dossier-inscription-2023-2024.pdf" class="btn btn-primary btn-lg" download>Télécharger le Dossier</a> -->
-                <!-- <a href="/fichiers/dossier-inscription-2023-2024.pdf" class="btn btn-primary btn-lg" download>Télécharger le Dossier</a> -->
-                <a href="/fichiers/XXXXX.pdf" class="btn btn-primary btn-lg disabled" download>Disponibilité prochaine</a>
+                <p><a href="/fichiers/cerfa_15699_01_quastionnaire_de_sante.pdf" class="btn btn-primary btn-lg" download>Questionnaire de santé (pdf)</a></p>
+                <p><a href="/fichiers/cm_attestation_fsgt_23_24.pdf" class="btn btn-primary btn-lg" download>Attestation FSGT (pdf)</a></p>
+                <p><a href="/fichiers/formulaire_inscription_23_24.doc" class="btn btn-primary btn-lg" download>Formulaire Inscription CIGOGNES 23/24 (word)</a></p>
+                <p><a href="/fichiers/infos_adherents_23_24.docx" class="btn btn-primary btn-lg" download>Infos Adhérents 23/24 (word)</a></p>
+
             </div>
         </div>
     </div>
